bb31b8c584261d2f0a6910ef62b17a970960abba
superhomenen <144682900+superhomenen@users.noreply.github.com>
2023-09-25 11:08:57 -0300

    fiz a tabela haha

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link rel="stylesheet" href="estilos/estilo.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>jogos</title>
+</head>
+<body>
+    <div id="corpo">
+    <h1>Escolha Um Jogo</h1>
+    <table class="listagem">
+        <tr><td>Nome <td>foto <td>adm
+        <tr><td>Nome <td>foto <td>adm
+        <tr><td>Nome <td>foto <td>adm
+        <tr><td>Nome <td>foto <td>adm
+    </table>
+    </div>
+</body>
+</html>
